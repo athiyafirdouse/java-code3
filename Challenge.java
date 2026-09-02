312eac355adffc1e628ed7ec3467d270f4c93d02
Athiya Firdouse <athiyafirdouseu.cse25@veltechmultitech.org>
2026-09-02 22:28:39 +0530

    adding the file

---
+++ Challenge.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Challenge {
+ public static void main(String[] args) {
+     System.out.println("please enter your name:");
+     Scanner sc =new Scanner(System.in);
+     String name= sc.nextLine();
+     System.out.println("welcome"+ name+"kg coding" );
+ }
+}
--- goodmorning.java
+++ goodmorning.java
@@ -2,6 +2,6 @@ public class goodmorning {
     public static void main(String[] args)
     {
         System.out.println("good morning");
-        
+    
     }
 }
