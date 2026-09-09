87f3e782514c0a628f3117b14649229d9229bc56
Athiya Firdouse <athiyafirdouseu.cse25@veltechmultitech.org>
2026-09-09 22:35:31 +0530

    adding the file

---
+++ prime.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class prime {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        System.out.println("enter the number");
+        int num = sc.nextInt();
+        boolean isPrime = isPrime(num);
+        if(isPrime)
+        {
+            System.out.println("your number is prime");
+
+        }
+        else{
+            System.out.println("your number  is not prime");
+        }
+    }
+    
+    public static boolean isPrime(int num)
+    {
+        int i=2;
+        while(i<num)
+        {
+         if(num % i ==0)
+         {
+            return false;
+         }
+         i++;
+        }
+   
+
+       return true;
+    }
+    
+}
