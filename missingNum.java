57a108a32df454c94a5c9596d4e8c20f2801983d
Athiya Firdouse <athiyafirdouseu.cse25@veltechmultitech.org>
2026-09-14 21:15:27 +0530

    adding into the file

---
+++ missingNum.java
@@ -0,0 +1,23 @@
+public class missingNum {
+ 
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 5};
+
+        int n = arr.length + 1;
+
+        int totalSum = n * (n + 1) / 2;
+
+        int arraySum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            arraySum = arraySum + arr[i];
+        }
+
+        int missingNumber = totalSum - arraySum;
+
+        System.out.println("Missing Number = " + missingNumber);
+    }
+}
+    
+
