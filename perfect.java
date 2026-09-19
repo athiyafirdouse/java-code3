b46c243325582c092015dd66998ddc9c4eb3911d
Athiya Firdouse <athiyafirdouseu.cse25@veltechmultitech.org>
2026-09-19 21:19:27 +0530

    adding into the file

---
+++ perfect.java
@@ -0,0 +1,30 @@
+ import java.util.Scanner;
+  
+ class Perfect
+{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum = sum + i;
+            }
+        }
+
+        if (sum == num) {
+            System.out.println("Perfect Number");
+        } else {
+            System.out.println("Not a Perfect Number");
+        }
+
+        sc.close();
+    }
+} 
+    
+
