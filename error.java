c880366a7ee08357ae46753b1888a9208feb859e
Athiya Firdouse <athiyafirdouseu.cse25@veltechmultitech.org>
2026-09-23 23:04:22 +0530

    adding into the file

---
+++ error.java
@@ -0,0 +1,18 @@
+class error
+{
+    public static void main(String[] args)
+    {
+        System.out.print("can you find the error");
+    }
+    class Anotherexample
+    {
+        public void insert()
+        {
+            System.out.print("to insert a text");
+        }
+    abstract void delete()
+    {
+        System.out.print("to delete a text");
+    }
+    }
+}
---
+++ runtime.java
@@ -0,0 +1,13 @@
+public class runtime {
+    public static void main(String[] args) {
+        
+    
+    int a= Integer.parseInt(args[5]);
+    int b = Integer.parseInt(args[5]);
+    int c = a/b;
+    System.out.println("value of c ="+c);
+
+    
+}
+
+}
